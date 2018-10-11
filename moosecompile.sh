842b9190640c681049b579bdbcbaa5c8b2f53a75
Andy <andy@andys-mbp.boisestate.edu>
2018-10-11 15:31:03 -0600

    moosecompile edit

--- moosecompile.sh
+++ moosecompile.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 16 10:24:30 2018
@@ -17,7 +18,7 @@ moose_mol_dir = os.path.join(moose_base,'modules')
 
 test_moose=os.path.exists(moose_base)
 # add exception for if moose is clone already
-if result == None:
+if test_moose is None:
     print("MOOSE doesn't exist")
     subprocess.call("git clone git@github.com:idaholab/moose.git", shell=True)
 os.chdir(moose_base)
