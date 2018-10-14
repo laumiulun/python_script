5806cc92b758ca3086b9362d1d7cb26a9b28dabc
Andy <andy@Andys-MacBook-Pro.local>
2018-10-14 09:30:16 -0600

    added help and command line arg

--- moosecompile.sh
+++ moosecompile.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 16 10:24:30 2018
@@ -11,19 +11,28 @@ To compile moose and its submodules directly
 import os
 import subprocess
 
+
 home_dir = os.getcwd()
 moose_base = os.path.join(home_dir,'moose')
 moose_mol_dir = os.path.join(moose_base,'modules')
 
+if "-h" in sys.argv:
+  print "-h = help"
+  print "-c = compile only"
+  print "-u = update_libmesh"
+# No need to reclone
+if "-c" in sys.argv:
+  test_moose=os.path.exists(moose_base)
+  # add exception for if moose is clone already
+  if test_moose is None:
+      print("MOOSE doesn't exist")
+      subprocess.call("git clone git@github.com:idaholab/moose.git", shell=True)
+  os.chdir(moose_base)
+  rebuild_libmesh=subprocess.call("./scripts/update_and_rebuild_libmesh.sh", shell=True)
 
-test_moose=os.path.exists(moose_base)
-# add exception for if moose is clone already
-if test_moose is None:
-    print("MOOSE doesn't exist")
-    subprocess.call("git clone git@github.com:idaholab/moose.git", shell=True)
-os.chdir(moose_base)
-rebuild_libmesh=subprocess.call("./scripts/update_and_rebuild_libmesh.sh", shell=True)
-
+elif "-u" in sys.argv:
+  os.chdir(moose_base)
+  rebuild_libmesh=subprocess.call("./scripts/update_and_rebuild_libmesh.sh", shell=True)
 os.chdir(os.path.join(moose_base,'test'))
 subprocess.call("make -j 4", shell=True)
 subprocess.call("./run_tests -j 4", shell=True)
